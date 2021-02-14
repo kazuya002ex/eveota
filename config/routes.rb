439ed07a6ab5195dd3752cf6a25433ff003070b6
kazuya002ex <chiharu.event2@gmail.com>
2021-02-14 22:29:36 +0900

    feat: static達のrouteを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
+
+  get '/help', to: 'static_pages#help'
+  get '/guide', to: 'static_pages#guide'
+  get '/privacy', to: 'static_pages#privacy'
+  get '/lp', to: 'static_pages#lp'
 end
