d677872980b58cbf69405cbda7f52877343b7e0a
kazuya002ex <chiharu.event2@gmail.com>
2021-02-15 13:55:36 +0900

    feat: 中身を記述

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,28 @@
 class EventsController < ApplicationController
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+    def event_params
+    end
+
+    def set_event
+    end
 end
