f99db4b891236e25e7053a157a67c3d7b2afaf79
kazuya002ex <chiharu.event2@gmail.com>
2021-02-15 11:53:50 +0900

    feat: Eventのマイグレーションファイルを作成

--- db/migrate/20210215024829_create_events.rb
+++ db/migrate/20210215024829_create_events.rb
@@ -1,8 +1,17 @@
 class CreateEvents < ActiveRecord::Migration[6.1]
   def change
     create_table :events do |t|
+      t.string :name, null: false
+      t.time :start_time, null: false, comment: "開演時間"
+      t.time :opening_time, null: false, comment: "開場時間"
+      t.date :performance_date, null: false, comment: "公演日"
+      t.references :venue, foreign_key: true, null: false
+      t.references :user, foreign_key: true, null: false
 
       t.timestamps
     end
+
+    add_index :events, :name, unique: true
+    add_index :performance_date
   end
 end
