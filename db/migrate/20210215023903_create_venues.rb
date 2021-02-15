2aacf8ce801af8abcb4365816d37b73df7b924a0
kazuya002ex <chiharu.event2@gmail.com>
2021-02-15 11:46:14 +0900

    feat: Venueのマイグレーションファイルを作成

--- db/migrate/20210215023903_create_venues.rb
+++ db/migrate/20210215023903_create_venues.rb
@@ -1,8 +1,17 @@
 class CreateVenues < ActiveRecord::Migration[6.1]
   def change
     create_table :venues do |t|
+      t.string :name, null: false, comment: "会場名"
+      t.string :station, null: false, comment: "最寄り駅名"
+      t.string :location, null: false, comment: "会場の住所"
+      t.integer :walk, null: false, limit: 3, comment: "徒歩時間"
+      t.float :latitude, null: false, comment: "緯度"
+      t.float :longitude, null: false, comment: "経度"
 
       t.timestamps
     end
+
+    add_index :venues, :station
+    add_index :venues, :name
   end
 end
