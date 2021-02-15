17324747b434c4205d90f9a6c46c00b5fc0f5b3c
kazuya002ex <chiharu.event2@gmail.com>
2021-02-15 12:18:33 +0900

    feat: seedに「さいたまスーパーアリーナ」の情報を追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,10 @@
-
+# Venue Create
+p 'Venue create'
+Venue.create!(
+  name: 'さいたまスーパーアリーナ',
+  station: 'さいたま新都心'
+  location: '埼玉県さいたま市中央区新都心8'
+  walk: 3
+  latitude: 35.894956
+  longitude: 139.630767
+)
