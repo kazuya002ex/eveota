4b7f5e087a16fa3407ba143587860543c421353e
kazuya002ex <chiharu.event2@gmail.com>
2021-02-15 19:18:35 +0900

    fix: 軽微

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,5 +1,5 @@
 class VenuesController < ApplicationController
-  before_action :set_event, only: [:show, :edit, :update, :destroy]
+  before_action :set_venue, only: [:show, :edit, :update, :destroy]
 
   def show
   end
