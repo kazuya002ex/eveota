7f59d35457b36b1d376e7a20e17dba1bfcd2be0a
kazuya002ex <chiharu.event2@gmail.com>
2021-02-15 13:41:07 +0900

    feat: Venueモデルのバリデーション追加

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,3 +1,10 @@
 class Venue < ApplicationRecord
   has_many :events
+
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :station, presence: true, length: { maximum: 30 }
+  validates :location, presence: true, length: { maximum: 65 }
+  validates :walk, presence: true, length: { maximum: 3 }
+  validates :latitude, presence: true
+  validates :longitude, presence: true
 end
