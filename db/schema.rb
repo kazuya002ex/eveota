449bbd528ac1b3fd75f361a71a7bd99d8e0f8390
kazuya002ex <chiharu.event2@gmail.com>
2021-02-15 12:33:24 +0900

    feat: スキーマ更新

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_15_024829) do
+ActiveRecord::Schema.define(version: 2021_02_15_033138) do
 
   create_table "events", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2021_02_15_024829) do
     t.float "longitude", null: false, comment: "経度"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "access_info"
   end
 
   add_foreign_key "events", "users"
