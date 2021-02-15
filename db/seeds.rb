e98a988cc53532422afedbfd191482015a11cc66
kazuya002ex <chiharu.event2@gmail.com>
2021-02-15 12:19:30 +0900

    fix: seedの書き方ミスってっぞ

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,8 @@ p 'Venue create'
 Venue.create!(
   name: 'さいたまスーパーアリーナ',
   station: 'さいたま新都心'
-  location: '埼玉県さいたま市中央区新都心8'
-  walk: 3
-  latitude: 35.894956
+  location: '埼玉県さいたま市中央区新都心8',
+  walk: 3,
+  latitude: 35.894956,
   longitude: 139.630767
 )
