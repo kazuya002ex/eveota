fe9a618bd771a2fd00c33ad96cc9b1608b9414c9
kazuya002ex <chiharu.event2@gmail.com>
2021-02-15 13:42:38 +0900

    feat: Eventモデルのバリデーション追加

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,9 @@
 class Event < ApplicationRecord
   belongs_to :venue
   belongs_to :user
+
+  validates :name, presence: true, length: { maximum: 55 }
+  validates :start_time, presence: true
+  validates :opening_time, presence: true
+  validates :performance_date, presence: true
 end
