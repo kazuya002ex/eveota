3b43dbd3f19462125751fbd628b9dd67938385a8
kazuya002ex <chiharu.event2@gmail.com>
2021-02-15 13:47:57 +0900

    feat: Venueコントローラーの中身を作るお

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,2 +1,28 @@
 class VenuesController < ApplicationController
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+    def venue_params
+    end
+
+    def set_venue
+    end
 end
