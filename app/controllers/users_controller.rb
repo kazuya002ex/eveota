11595b86ac4ef4f80105ff0df3a2463b73d85419
kazuya002ex <chiharu.event2@gmail.com>
2021-02-15 13:59:00 +0900

    feat: Userこんとろーらー作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+
+  def show
+  end
+
+  def new
+  end
+
+  def craete
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+    def user_params
+    end
+
+    def set_user
+    end
+end
