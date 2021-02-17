8531727a80676df59baa030adbe904de72be8d46
kazuya002ex <chiharu.event2@gmail.com>
2021-02-17 14:44:50 +0900

    feat: routes追加（users）

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   get '/guide', to: 'static_pages#guide'
   get '/privacy', to: 'static_pages#privacy'
   get '/lp', to: 'static_pages#lp'
+
+  resources :users, only: [:show, :new, :create, :edit, :update]
 end
